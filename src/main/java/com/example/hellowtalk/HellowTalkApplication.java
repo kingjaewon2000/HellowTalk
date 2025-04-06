94834ace867b72ce189d0c30bd2ee9be32ef7fdc
kingjaewon2000 <kkkxx_00@naver.com>
2025-04-06 21:45:51 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/example/hellowtalk/HellowTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellowtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellowtalk/HellowTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

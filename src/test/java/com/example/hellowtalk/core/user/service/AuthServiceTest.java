41d78d29450a2c6208e0e4621bc0f8f0faa3beba
kingjaewon2000 <kkkxx_00@naver.com>
2025-04-08 22:22:09 +0900

    feat: 로그인 및 회원가입 단위 테스트 작성

---
+++ src/test/java/com/example/hellowtalk/core/user/service/AuthServiceTest.java
@@ -0,0 +1,86 @@
+package com.example.hellowtalk.core.user.service;
+
+import com.example.hellowtalk.core.user.dto.request.LoginRequest;
+import com.example.hellowtalk.core.user.entity.User;
+import com.example.hellowtalk.core.user.repository.UserRepository;
+import com.example.hellowtalk.dummy.Dummy;
+import com.example.hellowtalk.global.auth.JwtProvider;
+import com.example.hellowtalk.global.exception.CustomException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private JwtProvider jwtProvider;
+
+    @InjectMocks
+    private AuthService authService;
+
+    private LoginRequest request;
+
+    @BeforeEach
+    void setUp() {
+        String username = "test";
+        String password = "test1234";
+        request = new LoginRequest(username, password);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 시 액세스 토큰을 반환한다")
+    void login() {
+        // given
+        User user = Dummy.mockUser();
+
+        when(userRepository.findByUsername(request.username())).thenReturn(user);
+        when(jwtProvider.createAccessToken(user.getUserId(), user.getUsername())).thenReturn("token");
+
+        // when
+        String token = authService.login(request);
+
+        // then
+        assertThat(token).isNotBlank();
+    }
+
+    @Test
+    @DisplayName("로그인 시 존재하지 않 아이디(username)을 입력하면 예외를 던진다")
+    void usernameNotFound() {
+        // given
+        when(userRepository.findByUsername(request.username())).thenReturn(null);
+
+        // when
+        assertThatThrownBy(() -> authService.login(request))
+                .isInstanceOf(CustomException.class);
+
+        // then
+    }
+
+    @Test
+    @DisplayName("로그인 시 비밀번호 틀리면 예외를 던진다")
+    void passwordIsNotMatch() {
+        // given
+        LoginRequest failRequest = new LoginRequest(request.username(), "fail");
+
+        when(userRepository.findByUsername(request.username())).thenReturn(Dummy.mockUser());
+
+        // when
+        assertThatThrownBy(() -> authService.login(failRequest))
+                .isInstanceOf(CustomException.class);
+
+        // then
+
+    }
+
+}
---
+++ src/test/java/com/example/hellowtalk/dummy/Dummy.java
@@ -0,0 +1,21 @@
+package com.example.hellowtalk.dummy;
+
+import com.example.hellowtalk.core.user.entity.LoginStatus;
+import com.example.hellowtalk.core.user.entity.User;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.time.LocalDateTime;
+
+public class Dummy {
+
+    public static User mockUser() {
+        return User.builder()
+                .userId(1L)
+                .username("test")
+                .password(BCrypt.hashpw("test1234", BCrypt.gensalt()))
+                .name("테스트 계정")
+                .status(LoginStatus.OFFLINE)
+                .lastLoginAt(LocalDateTime.now())
+                .build();
+    }
+}
